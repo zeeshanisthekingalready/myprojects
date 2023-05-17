f89f31861f87e142546c2d86bfc8c0ff69fc9141
zeeshanisthekingalready <133774747+zeeshanisthekingalready@users.noreply.github.com>
2023-05-17 13:49:33 +0400

    Add files via upload

---
+++ square.py
@@ -0,0 +1,101 @@
+Python 3.10.11 (v3.10.11:7d4cc5aa85, Apr  4 2023, 19:05:19) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+Welcome to my password genrator.
+
+How many charecters do you want your password to become.
+
+12
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+Welcome to my password genrator.
+
+How many charecters do you want your password to become.
+
+12
+
+
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+Welcome to my password genrator.
+
+How many charecters do you want your password to become.
+
+12
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+Welcome to my password genrator.
+
+How many charecters do you want your password to become.
+
+12
+r 
+
+x 
+
+q 
+
+x 
+
+o 
+
+y 
+
+q 
+
+c 
+
+l 
+
+o 
+
+f 
+
+t 
+
+
+============ RESTART: /Users/riyaskayyalakkandi/Desktop/password.py ============
+Welcome to my password genrator.
+
+How many charecters do you want your password to become.
+
+12
+u
+g
+o
+t
+p
+f
+h
+q
+w
+n
+q
+p
+>>> 
+=========== RESTART: /Users/riyaskayyalakkandi/Desktop/tic-tac-toe.py ==========
+Welcome to tic-tac-toe.
+
+|1|2|3|
+|4|5|6|
+|7|8|9|
+
+>>> 
+============ RESTART: /Users/riyaskayyalakkandi/Documents/square.py ============
+Traceback (most recent call last):
+  File "/Users/riyaskayyalakkandi/Documents/square.py", line 2, in <module>
+    for i in rand(0,4,1):
+NameError: name 'rand' is not defined. Did you mean: 'round'?
+>>> 
+============ RESTART: /Users/riyaskayyalakkandi/Documents/square.py ============
+Traceback (most recent call last):
+  File "/Users/riyaskayyalakkandi/Documents/square.py", line 2, in <module>
+    for i in rand(0,4,1):
+NameError: name 'rand' is not defined. Did you mean: 'round'?
+>>> 
+============ RESTART: /Users/riyaskayyalakkandi/Documents/square.py ============
